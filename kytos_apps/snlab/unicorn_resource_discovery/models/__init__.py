a1c22fe0c8e464f080db705c552e7ab63c8a65a5
Qiao Xiang <xiangq27@gmail.com>
2017-10-10 18:54:41 -0400

    rm ipv4 and port model

--- kytos_apps/snlab/unicorn_resource_discovery/models/__init__.py
+++ kytos_apps/snlab/unicorn_resource_discovery/models/__init__.py
@@ -8,9 +8,9 @@ from .ane_flow_vector import ANEFlowVector
 from .error_response import ErrorResponse
 from .error_response_meta import ErrorResponseMeta
 from .flow_spec import FlowSpec
-from .ipv4_addr import Ipv4Addr
+#from .ipv4_addr import Ipv4Addr
 from .path_query_response import PathQueryResponse
-from .port_number import PortNumber
+#from .port_number import PortNumber
 from .query_desc import QueryDesc
 from .query_requests import QueryRequests
 from .resource_query_response import ResourceQueryResponse
--- kytos_apps/snlab/unicorn_resource_discovery/models/path_query_response.py
+++ kytos_apps/snlab/unicorn_resource_discovery/models/path_query_response.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 
 from __future__ import absolute_import
-from napps.snlab.unicorn_resource_discovery.models.ipv4_addr import Ipv4Addr
 from .base_model_ import Model
 from datetime import date, datetime
 from typing import List, Dict
